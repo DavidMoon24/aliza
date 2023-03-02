8fb051d2bfbf4f6084c1116956df1675caf8a117
DavidMoon24 <124087158+DavidMoon24@users.noreply.github.com>
2023-03-02 18:35:23 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="utf-8">
+	<title>Coro Aliza - Iniciar Sesión</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" type="image/jpeg" href="alizaicono.jpg">
+</head>
